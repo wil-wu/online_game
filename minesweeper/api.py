9f2c78d5e1fef28f673a89e8ed98bc56702e61d8
wil-wu <1850836076@qq.com>
2023-11-09 11:21:13 +0800

    convert view function to view class

--- minesweeper/api.py
+++ minesweeper/api.py
@@ -1,6 +1,7 @@
 import random
 
 from flask import Blueprint, request, url_for, redirect, session, g, current_app
+from flask.views import MethodView
 from sqlalchemy import desc
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -12,122 +13,135 @@ from minesweeper.decorators import login_required
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 
-@bp.route('/login', methods=('POST',))
-def login() -> AjaxData:
+class APIView(MethodView):
+    init_every_request = False
+    decorators = [login_required]
+
+
+class LoginAPI(MethodView):
     """
     登录验证
-    :return:
     """
-    form = LoginForm()
-    if not form.validate_on_submit():
-        return AjaxData(400, '格式错误', form.errors)
+    init_every_request = False
 
-    error = None
-    username = form.data['username']
-    password = form.data['password']
-    user = User.query.filter_by(username=username).first()
+    def post(self) -> AjaxData:
+        form = LoginForm()
+        if not form.validate_on_submit():
+            return AjaxData(400, '格式错误', form.errors)
 
-    if user is None:
-        error = {'username': ['用户名不存在']}
+        error = None
+        username = form.data['username']
+        password = form.data['password']
+        user = User.query.filter_by(username=username).first()
 
-    elif not check_password_hash(user.password, password):
-        error = {'password': ['密码错误']}
+        if user is None:
+            error = {'username': ['用户名不存在']}
 
-    if error is not None:
-        return AjaxData(400, '登录失败', error)
+        elif not check_password_hash(user.password, password):
+            error = {'password': ['密码错误']}
 
-    session['user_id'] = user.user_id
-    return AjaxData(msg='登录成功')
+        if error is not None:
+            return AjaxData(400, '登录失败', error)
+
+        session['user_id'] = user.user_id
+        return AjaxData(msg='登录成功')
 
 
-@bp.route('/register', methods=('POST',))
-def register() -> AjaxData:
+class RegisterAPI(MethodView):
     """
     注册验证
-    :return:
     """
-    form = RegisterForm()
-    if not form.validate_on_submit():
-        return AjaxData(400, '格式错误', form.errors)
+    init_every_request = False
 
-    username = form.data['username']
-    password = form.data['password']
+    def post(self) -> AjaxData:
+        form = RegisterForm()
+        if not form.validate_on_submit():
+            return AjaxData(400, '格式错误', form.errors)
 
-    if User.query.filter_by(username=username).scalar():
-        error = {'username': ['用户名已存在']}
-        return AjaxData(400, '注册失败', error)
+        username = form.data['username']
+        password = form.data['password']
 
-    user = User(username=username, password=generate_password_hash(password))
-    db.session.add(user)
-    db.session.commit()
-    return AjaxData(msg='注册成功')
+        if User.query.filter_by(username=username).scalar():
+            error = {'username': ['用户名已存在']}
+            return AjaxData(400, '注册失败', error)
+
+        user = User(username=username, password=generate_password_hash(password))
+        db.session.add(user)
+        db.session.commit()
+        return AjaxData(msg='注册成功')
 
 
-@bp.route('/logout')
-def logout():
+class LogoutAPI(MethodView):
     """
     退出登录
     """
-    session.clear()
-    return redirect(url_for('views.auth'))
+    init_every_request = False
+
+    def get(self):
+        session.clear()
+        return redirect(url_for('views.auth'))
 
 
-@bp.route('/map')
-@login_required
-def gen_game_map() -> AjaxData:
+class GameMapAPI(APIView):
     """
     游戏地图
-    :return:
     """
-    form = SpecForm(**request.args)
-    if not form.validate():
-        return AjaxData(400, '格式错误', form.errors)
+    def get(self) -> AjaxData:
+        form = SpecForm(**request.args)
+        if not form.validate():
+            return AjaxData(400, '格式错误', form.errors)
 
-    width = form.data['width']
-    height = form.data['height']
+        width = form.data['width']
+        height = form.data['height']
 
-    mine_idx = set()
-    mine_count = int(width * height * current_app.config['MINE_RATE'])
-    raw_map = [[0] * (width + 2) for _ in range(height + 2)]  # 将地图扩大一圈，计算周围雷数可避免边界情况
-    rendered_map = [[0] * width for _ in range(height)]
+        mine_idx = set()
+        mine_count = int(width * height * current_app.config['MINE_RATE'])
+        raw_map = [[0] * (width + 2) for _ in range(height + 2)]  # 将地图扩大一圈，计算周围雷数可避免边界情况
+        rendered_map = [[0] * width for _ in range(height)]
 
-    # 计算单元格周围地雷数
-    def get_around_mines(r: int, c: int) -> int:
-        # 上，下，左，右
-        x = raw_map[r - 1][c] + raw_map[r + 1][c] + raw_map[r][c - 1] + raw_map[r][c + 1]
-        # 左上，右上，左下，右下
-        y = raw_map[r - 1][c - 1] + raw_map[r - 1][c + 1] + raw_map[r + 1][c - 1] + raw_map[r + 1][c + 1]
-        return x + y
+        # 计算单元格周围地雷数
+        def get_around_mines(r: int, c: int) -> int:
+            # 上，下，左，右
+            x = raw_map[r - 1][c] + raw_map[r + 1][c] + raw_map[r][c - 1] + raw_map[r][c + 1]
+            # 左上，右上，左下，右下
+            y = raw_map[r - 1][c - 1] + raw_map[r - 1][c + 1] + raw_map[r + 1][c - 1] + raw_map[r + 1][c + 1]
+            return x + y
 
-    # 随机填充地雷
-    while mine_count > 0:
-        idx = random.randrange(0, width * height)
-        if idx not in mine_idx:
-            row = idx // width + 1
-            col = idx % width + 1
-            raw_map[row][col] = 1
-            mine_count -= 1
-            mine_idx.add(idx)
+        # 随机填充地雷
+        while mine_count > 0:
+            idx = random.randrange(0, width * height)
+            if idx not in mine_idx:
+                row = idx // width + 1
+                col = idx % width + 1
+                raw_map[row][col] = 1
+                mine_count -= 1
+                mine_idx.add(idx)
 
-    # 计算每个格子周围地雷数
-    for i in range(height):
-        for j in range(width):
-            rendered_map[i][j] = get_around_mines(i + 1, j + 1) if raw_map[i + 1][j + 1] == 0 else 9
+        # 计算每个格子周围地雷数
+        for i in range(height):
+            for j in range(width):
+                rendered_map[i][j] = get_around_mines(i + 1, j + 1) if raw_map[i + 1][j + 1] == 0 else 9
 
-    return AjaxData(data=rendered_map)
+        return AjaxData(data=rendered_map)
 
 
-@bp.route('/history', methods=('GET', 'POST'))
-@login_required
-def game_history() -> AjaxData:
+class HistoryAPI(APIView):
     """
     游戏记录
-    :return:
     """
-    if request.method == 'POST':
+    def get(self) -> AjaxData:
+        max_per_page = current_app.config['MAX_PER_PAGE']
+        pagination = Record.query.filter_by(user_id=g.user.user_id).order_by(desc('playdate')).paginate(
+            max_per_page=max_per_page,
+            error_out=False
+        )
+        return AjaxData(data={'pages': pagination.pages, 'items': pagination.items})
+
+    def post(self) -> AjaxData:
         form = RecordForm()
         if not form.validate_on_submit():
             return AjaxData(400, '格式错误', form.errors)
+
         data = form.data
         data.pop('csrf_token', None)
         record = Record(**data, user_id=g.user.user_id)
@@ -135,39 +149,32 @@ def game_history() -> AjaxData:
         db.session.commit()
         return AjaxData(msg='已保存游戏记录')
 
-    max_per_page = current_app.config['MAX_PER_PAGE']
-    pagination = Record.query.filter_by(user_id=g.user.user_id).order_by(desc('playdate')).paginate(
-        max_per_page=max_per_page,
-        error_out=False
-    )
-    return AjaxData(data={'pages': pagination.pages, 'items': pagination.items})
+
+class SingleHistoryAPI(APIView):
+    """
+    游戏记录详情
+    """
+    def get(self, record_id) -> AjaxData:
+        record = db.session.get(Record, record_id)
+        if record is None:
+            return AjaxData(400, '记录不存在')
+        if record.user_id != g.user.user_id:
+            return AjaxData(400, '无权限访问')
+        return AjaxData(data=record)
 
 
-@bp.route('/history/<int:record_id>')
-@login_required
-def single_history(record_id: int) -> AjaxData:
-    record = db.session.get(Record, record_id)
-    if record is None:
-        return AjaxData(400, '记录不存在')
-    if record.user_id != g.user.user_id:
-        return AjaxData(400, '无权限访问')
-    return AjaxData(data=record)
-
-
-@bp.route('/rank')
-@login_required
-def game_rank() -> AjaxData:
+class RankAPI(APIView):
     """
     游戏排行榜
-    :return:
     """
-    form = SpecForm(**request.args)
-    if not form.validate():
-        return AjaxData(400, '格式错误', form.errors)
+    def get(self) -> AjaxData:
+        form = SpecForm(**request.args)
+        if not form.validate():
+            return AjaxData(400, '格式错误', form.errors)
 
-    rank_limit = current_app.config['RANK_LIMIT']
-    rank = Record.query.filter_by(**form.data).order_by('remainder', 'playtime').limit(rank_limit).all()
-    return AjaxData(data=rank)
+        rank_limit = current_app.config['RANK_LIMIT']
+        rank = Record.query.filter_by(**form.data).order_by('remainder', 'playtime').limit(rank_limit).all()
+        return AjaxData(data=rank)
 
 
 @bp.before_app_request
@@ -178,3 +185,12 @@ def before_request():
         g.user = None
     else:
         g.user = db.session.get(User, user_id)
+
+
+bp.add_url_rule('/login', view_func=LoginAPI.as_view('login'))
+bp.add_url_rule('/register', view_func=RegisterAPI.as_view('register'))
+bp.add_url_rule('/logout', view_func=LogoutAPI.as_view('logout'))
+bp.add_url_rule('/map', view_func=GameMapAPI.as_view('map'))
+bp.add_url_rule('/history', view_func=HistoryAPI.as_view('history'))
+bp.add_url_rule('/history/<int:record_id>', view_func=HistoryAPI.as_view('single_history'))
+bp.add_url_rule('/rank', view_func=RankAPI.as_view('rank'))
--- minesweeper/views.py
+++ minesweeper/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Response, render_template
+from flask.views import MethodView
 from flask_wtf.csrf import generate_csrf
 
 from minesweeper.decorators import login_required
@@ -6,21 +7,36 @@ from minesweeper.decorators import login_required
 bp = Blueprint('views', __name__)
 
 
-@bp.route('/auth')
-def auth():
-    return render_template('auth.html')
+class AuthView(MethodView):
+    """
+    登录/注册视图
+    """
+    init_every_request = False
+
+    def get(self):
+        return render_template('auth.html')
 
 
-@bp.route('/game')
-@login_required
-def game():
-    return render_template('game.html')
+class GameView(MethodView):
+    """
+    游戏视图
+    """
+    init_every_request = False
+    decorators = [login_required]
+
+    def get(self):
+        return render_template('game.html')
 
 
-@bp.route('/record')
-@login_required
-def record():
-    return render_template('record.html')
+class RecordView(MethodView):
+    """
+    游戏记录视图
+    """
+    init_every_request = False
+    decorators = [login_required]
+
+    def get(self):
+        return render_template('record.html')
 
 
 @bp.after_request
@@ -28,3 +44,8 @@ def after_request(response: Response) -> Response:
     csrf_token = generate_csrf()
     response.set_cookie('csrftoken', csrf_token)
     return response
+
+
+bp.add_url_rule('/auth', view_func=AuthView.as_view('auth'))
+bp.add_url_rule('/game', view_func=GameView.as_view('game'))
+bp.add_url_rule('/record', view_func=RecordView.as_view('record'))
